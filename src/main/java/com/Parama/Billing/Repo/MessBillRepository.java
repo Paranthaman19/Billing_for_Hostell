118ec545e7298e90b2640e8e3f2fbcbd0b86f0d0
Paranthaman <146100982+Paranthaman19@users.noreply.github.com>
2024-10-21 09:57:56 +0530

    Update MessBillRepository.java

--- src/main/java/com/Parama/Billing/Repo/MessBillRepository.java
+++ src/main/java/com/Parama/Billing/Repo/MessBillRepository.java
@@ -7,6 +7,6 @@ import com.Parama.Billing.Entity.MessBill;
 
 @Repository
 public interface MessBillRepository extends JpaRepository<MessBill, Long> {
-    // Custom query to fetch the latest MessBill based on the highest id
+   
     MessBill findTopByOrderByIdDesc();
 }
